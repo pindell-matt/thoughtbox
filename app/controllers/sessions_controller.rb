1f7cd437167a1cf5bb95f0f410a8f99cd4ba6b52
Matt Pindell <pindell.m@gmail.com>
2016-07-25 09:16:03 -0600

    Create sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+
+  def new
+    # will go to form
+  end
+
+  def create
+    # create new user session
+  end
+
+  def destroy
+    # logout
+  end
+
+end
