9a2d9439b311a692ed5bd4c29a42545443cab7fd
Matt Pindell <pindell.m@gmail.com>
2016-07-25 12:10:16 -0600

    Add user sign up test

---
+++ spec/features/user_sign_up_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "User can sign up" do
+  scenario "with valid email and password" do
+    visit root_path
+
+    within(".navbar") do
+      expect(page).to have_link("Log In")
+      expect(page).to have_link("Sign Up")
+      expect(page).to_not have_link("Log Out")
+
+      click_link("Sign Up")
+    end
+
+    within(".new_user_form") do
+      fill_in "email address", with: "fake@email.com"
+      fill_in "password", with: "password"
+      fill_in "confirm password", with: "password"
+      click_button("Sign Up")
+    end
+
+    expect(page).to have_current_path('/links')
+
+    within(".navbar") do
+      expect(page).to_not have_link("Log In")
+      expect(page).to_not have_link("Sign Up")
+      expect(page).to have_link("Log Out")
+    end
+  end
+end
