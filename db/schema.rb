714743f62553205459c397bd58a1683fe42bd926
Matt Pindell <pindell.m@gmail.com>
2016-07-25 09:22:43 -0600

    Adjust user to require only email and password

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20160725145737) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
     t.string   "email"
     t.string   "password"
     t.string   "password_digest"
