98d67d4580e386ed02054134cc54bd07dab6e0b5
Matt Pindell <pindell.m@gmail.com>
2016-07-25 10:31:34 -0600

    Create links controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params.dig("session", "email"))
     if @user && @user.authenticate(params.dig("session", "password"))
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to links_path
     else
       render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   get  '/users/new', to: 'users#new', as: :new_user
   post '/users',     to: 'users#create'
 
+  get '/links', to: 'links#index'
+
 end
