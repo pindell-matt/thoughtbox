7806b0756e422b03f45abff040bc082058afee2e
Matt Pindell <pindell.m@gmail.com>
2016-07-25 09:01:03 -0600

    Initial commit

---
+++ db/migrate/20160725145737_create_user.rb
@@ -0,0 +1,13 @@
+class CreateUser < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password
+      t.string :password_digest
+      t.string :password_confirmation
+
+      t.timestamps null: false
+    end
+  end
+end
