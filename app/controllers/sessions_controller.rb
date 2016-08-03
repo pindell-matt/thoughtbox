474e1cba2a19c7a9b2ed5a28e9c2f18cab0a97f0
Matt Pindell <pindell.m@gmail.com>
2016-08-03 13:53:00 -0600

    Refactor session controller to use strong params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params.dig("session", "email"))
-    if @user && @user.authenticate(params.dig("session", "password"))
+    @user = User.find_by(email: session_params[:email])
+    if @user && @user.authenticate(session_params[:password])
       session[:user_id] = @user.id
       redirect_to links_path
     else
@@ -17,5 +17,11 @@ class SessionsController < ApplicationController
     session.clear
     redirect_to login_path
   end
+
+  private
+
+    def session_params
+      params.require(:session)
+    end
 
 end
